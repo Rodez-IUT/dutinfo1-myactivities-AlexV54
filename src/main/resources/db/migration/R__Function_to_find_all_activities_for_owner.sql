339d6d252b562b3a44d0a3462d5a9a89e963af1e
Alexandre Vergé <alexandre.verge@iut-rodez.fr>
2019-03-06 16:00:33 +0100

    Fix 3.2 function to find all activities for a given owner

---
+++ src/main/resources/db/migration/R__Function_to_find_all_activities_for_owner.sql
@@ -0,0 +1,3 @@
+CREATE or REPLACE FUNCTION find_all_activities_for_owner(ownername varchar(500)) RETURNS SETOF activity AS $$
+    SELECT act.* FROM activity act JOIN "user" owner ON owner_id=owner.id WHERE owner.username = ownername; 
+$$ LANGUAGE SQL;
